f9ce9649f003f6581e5384f581404baee05d9747
Isacc Alvarez <respaldoisaccalvarez@gmail.com>
2026-08-22 10:38:23 -0600

    feat: Add ApplicationDbContextFactory and update project references

--- src/HabitsApp.Presentation/HabitsApp.Api/Program.cs
+++ src/HabitsApp.Presentation/HabitsApp.Api/Program.cs
@@ -104,6 +104,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+await using (var scope = app.Services.CreateAsyncScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
